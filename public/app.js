c31680577c31d58213b66018f5c32084ce186da5
Proxyfetus <qianqianinfo@gmail.com>
2022-01-18 17:39:08 +0000

    responsive nav

--- public/app.js
+++ public/app.js
@@ -1,17 +1,34 @@
-const navSlide= () => {
+const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navlinks');
-    const navLinks = document.querySelectorAll('.navlinks li');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('.nav-active');
 
-        navLinks.forEach((link,index)=> {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2.3}s`;
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+            
         })
+        
+        burger.classList.toggle('toggle');
+
     })
 
-    const app = () => {
-        navSlide();
-    }
+    
+
+
 }
+
+navSlide();
+
+// const app = () => {
+//     navSlide();
+// }
+
+// app();
