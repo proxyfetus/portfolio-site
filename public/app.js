ca66effd890bca9bc03c8be295a25e7931801328
Proxyfetus <qianqianinfo@gmail.com>
2022-01-18 14:40:26 +0000

    adding js

--- public/app.js
+++ public/app.js
@@ -1,3 +1,17 @@
-cosnt = () => {
-    
+const navSlide= () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('.nav-active');
+
+        navLinks.forEach((link,index)=> {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2.3}s`;
+        })
+    })
+
+    const app = () => {
+        navSlide();
+    }
 }
